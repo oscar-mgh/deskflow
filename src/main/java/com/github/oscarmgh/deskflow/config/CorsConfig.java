bf0effe7a1fc773ae2ccb7d6fe8c9427ba026e89
oscar galindo <oscar.galindo1@yahoo.com>
2026-01-04 02:35:00 -0600

    cors updated

--- src/main/java/com/github/oscarmgh/deskflow/config/CorsConfig.java
+++ src/main/java/com/github/oscarmgh/deskflow/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:4200")
+                        .allowedOriginPatterns("https://deskflow.fly.dev")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true);
